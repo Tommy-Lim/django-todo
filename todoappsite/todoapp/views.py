bf936891d4f0bd675982a9fec3be9ca915e378a9
Tommy-Lim <tlim24@gmail.com>
2017-02-07 16:58:53 -0800

    todo page displays list items

--- todoappsite/todoapp/views.py
+++ todoappsite/todoapp/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from .models import Todo
 
 # Create your views here.
 def index(request):
-    # context = {
-    #     "todos":
-    # }
-    return render(request, 'todoapp/index.html')
+    context = {
+        "todos": Todo.objects.all()
+    }
+    return render(request, 'todoapp/index.html', context)
